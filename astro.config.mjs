e2c237b15976079922b739f2b2b6c3e06e99b11c
Thanapat Nartthanarung <thanapat2027@gmail.com>
2026-05-13 17:31:52 +0700

    Implemented homepage and projects page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,20 @@
 // @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
+import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwind()]
   },
+
   fonts: [{
     provider: fontProviders.fontsource(),
     name: 'Poppins',
     cssVariable: '--font-poppins',
-  }]
+  }],
+
+  integrations: [icon({
+    iconDir: "src/icons",
+  })]
 });
