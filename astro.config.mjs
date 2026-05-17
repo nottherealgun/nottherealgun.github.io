326fa80980da04c520171a05e844cfa0eb17015c
Thanapat Nartthanarung <thanapat2027@gmail.com>
2026-05-17 18:11:18 +0700

    Setup github deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwind from "@tailwindcss/vite";
 import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nottherealgun.github.io",
+
   vite: {
     plugins: [tailwind()],
   },
